5cc349ec16feb3d8cfb515701b9f0cb3f227b94d
AF <43990041+hygge@users.noreply.github.com>
2018-11-19 17:55:06 +0200

    First

---
+++ script.js
@@ -0,0 +1,12 @@
+const script2 = require('./script2.js');
+
+const a = script2.largeNumber;
+const b = 5;
+
+// console.log(a+b);	
+
+const c = require('fs');
+
+console.log(b);
+
+// fs and http modules - most common
